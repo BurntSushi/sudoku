a8357de3b58abe05aa4f3acaf88e0cf75d70fa8a
Norman Ramsey <nr@cs.tufts.edu>
2012-11-01 17:21:49 -0400

    dumbass extend in 8 lines

--- Solve.hs
+++ Solve.hs
@@ -5,13 +5,24 @@ where
 
 import Grid
 
-extend :: Grid -> [Grid]
-extend = error "extend not implemented"
 
 
 improve :: [Grid] -> [Grid]
 improve gs = if any full gs then filter full gs
              else (improve . filter (not . deadGrid) . concat . map extend) gs
+
+
+extend :: Grid -> [Grid]
+extend (Grid rows) = map Grid (extendRows rows)
+    where extendRows (row:rows) =
+              case extendRow row of
+                [] -> map (row:) (extendRows rows)
+                rows' -> [row:rows | row <- rows']
+          extendRows [] = error "tried to extend a full grid"
+
+          extendRow (Cell Nothing : cells) = [Cell (Just n) : cells | n <- [1..9]]
+          extendRow (c : cells) = [c : cells' | cells' <- extendRow cells]
+
 
 
 --------------------------
